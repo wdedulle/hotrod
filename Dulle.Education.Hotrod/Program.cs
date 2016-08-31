335f068a4baeb20b8f80049b1c966f373cac2f3c
dedulle wim <dedullewim@hotmail.com>
2016-08-31 21:24:13 +0200

    first time commit

---
+++ Dulle.Education.Hotrod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infinispan.HotRod;
+
+namespace Dulle.Education.Hotrod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
