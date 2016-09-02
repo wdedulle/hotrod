5ca75c45b574372765a37f01f20966c25bfada15
Dedulle Wim <WDEDUL01@jetair.be>
2016-09-02 17:44:17 +0200

    Person object COJO

---
+++ Dulle.Education.Hotrod/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dulle.Education.Hotrod
+{
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+        
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person [Name={Name + " " + FirstName}, Age={Age}]";
+        }
+    }
+}
