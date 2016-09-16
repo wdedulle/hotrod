e337a6423143d4f4c99a322aea3c7a647ffbe65a
Dedulle Wim <WDEDUL01@jetair.be>
2016-09-16 11:51:49 +0200

    query data remotely, using google protocol buffers for serzialization

--- Dulle.Education.Hotrod/Person.cs
+++ Dulle.Education.Hotrod/Person.cs
@@ -1,23 +1,186 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Google.Protobuf;
+using Google.Protobuf.Reflection;
 
 namespace Dulle.Education.Hotrod
 {
-    [Serializable]
-    class Person
+    internal class Person : IMessage<Person>
     {
-        public string Name { get; set; }
+        public int Id;
+        public string FirstName;
+        public string Surname;
+        public int Age;
+        private static readonly MessageParser<Person> _parser = new MessageParser<Person>(() => new Person());
 
-        public string FirstName { get; set; }
-        
-        public int Age { get; set; }
+        public static MessageParser<Person> Parser { get { return _parser; } }
+
+        public Person()
+        {
+            this.Id = -1;
+            this.FirstName = "";
+            this.Surname = "";
+            this.Age = -1;
+        }
+
+        public MessageDescriptor Descriptor
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int CalculateSize()
+        {
+            int num = 0;
+            if (this.Id > 0)
+            {
+                num += 1 + CodedOutputStream.ComputeInt32Size(this.Id);
+            }
+            if (this.Age > 0)
+            {
+                num += 1 + CodedOutputStream.ComputeInt32Size(this.Age);
+            }
+            if (this.FirstName.Length > 0)
+            {
+                num += 1 + CodedOutputStream.ComputeStringSize(this.FirstName);
+            }
+            if (this.Surname.Length > 0)
+            {
+                num += 1 + CodedOutputStream.ComputeStringSize(this.Surname);
+            }
+
+            return num;
+
+        }
+
+
+
+        public Person Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(Person other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            if (other != this)
+            {
+                if (this.Id != other.Id)
+                {
+                    return false;
+                }
+                if (this.Age != other.Age)
+                {
+                    return false;
+                }
+                if (this.FirstName != other.FirstName)
+                {
+                    return false;
+                }
+                if (this.Surname != other.Surname)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        public void MergeFrom(CodedInputStream input)
+        {
+            uint num;
+            while ((num = input.ReadTag()) > 0)
+            {
+                switch (num)
+                {
+                    case 34:
+                        this.FirstName = input.ReadString();
+                        break;
+
+                    case 26:
+                        this.Surname = input.ReadString();
+                        break;
+
+                    case 8:
+                        this.Id = input.ReadInt32();
+                        break;
+                    case 16:
+                        this.Age = input.ReadInt32();
+                        break;
+
+                    default:
+                        input.SkipLastField();
+                        break;
+                }
+            }
+        }
+
+        public void MergeFrom(Person other)
+        {
+            if (other != null)
+            {
+                if (other.Id > 0)
+                {
+                    this.Id = other.Id;
+                }
+                if (other.Age > 0)
+                {
+                    this.Age = other.Age;
+                }
+                if (other.Surname.Length > 0)
+                {
+                    this.Surname = other.Surname;
+                }
+                if (other.FirstName.Length > 0)
+                {
+                    this.FirstName = other.FirstName;
+                }
+            }
+        }
+
+        public void WriteTo(CodedOutputStream output)
+        {
+            if (this.Id > 0)
+            {
+                output.WriteRawTag(8);
+                output.WriteInt32(this.Id);
+            }
+            if (this.Age > 0)
+            {
+                output.WriteRawTag(16);
+                output.WriteInt32(this.Age);
+
+            }
+
+            if (this.Surname.Length > 0)
+            {
+                output.WriteRawTag(26);
+                output.WriteString(this.Surname);
+            }
+
+            if (this.FirstName.Length > 0)
+            {
+                output.WriteRawTag(34);
+                output.WriteString(this.FirstName);
+            }
+
+
+        }
 
         public override string ToString()
         {
-            return $"Person [Name={Name + " " + FirstName}, Age={Age}]";
+            return "#####################\n" +
+                  "User ID: " + this.Id + "\n" +
+                  "Firstname: " + this.FirstName + "\n" +
+                  "Surname: " + this.Surname + "\n" +
+                  "Age: " + this.Age + "\n" +
+                  "#####################\n"
+                  ;
         }
+
     }
 }
--- Dulle.Education.Hotrod/Program.cs
+++ Dulle.Education.Hotrod/Program.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Google.Protobuf;
+using Infinispan.HotRod;
+using Infinispan.HotRod.Config;
+using Org.Infinispan.Protostream;
+using Org.Infinispan.Query.Remote.Client;
+using Dulle.Education.Hotrod;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using Infinispan.HotRod.Impl;
-using Infinispan.HotRod.Config;
-using Infinispan.HotRod;
-using Org.Infinispan.Query.Remote.Client;
-using System.IO;
 
 namespace Dulle.Education.Hotrod
 {
@@ -15,38 +18,61 @@ namespace Dulle.Education.Hotrod
     {
         static void Main(string[] args)
         {
+            RemoteCacheManager remoteManager;
+            const string ERRORS_KEY_SUFFIX = ".errors";
+            const string PROTOBUF_METADATA_CACHE_NAME = "___protobuf_metadata";
+
             ConfigurationBuilder builder = new ConfigurationBuilder();
-            builder.AddServer()                                
-                .Host(args.Length > 1 ? args[0] : "127.0.0.1")
-                .Port(args.Length > 2 ? int.Parse(args[1]) : 11222)                
-                .ValueSizeEstimate(1073741824);
+            builder.AddServer()
+                  .Host(args.Length > 1 ? args[0] : "127.0.0.1")
+                  .Port(args.Length > 2 ? int.Parse(args[1]) : 11222);
 
-            
-            Configuration config = builder.Build();
-            
-            RemoteCacheManager cacheManager = new RemoteCacheManager(config);
-            
-            if (cacheManager.IsStarted())
+            builder.Marshaller(new BasicTypesProtoStreamMarshaller());
+            remoteManager = new RemoteCacheManager(builder.Build(), true);
+
+            IRemoteCache<String, String> metadataCache = remoteManager.GetCache<String, String>(PROTOBUF_METADATA_CACHE_NAME);
+            IRemoteCache<int, Person> testCache = remoteManager.GetCache<int, Person>("namedCache");
+
+            // Installing the entities model into the Infinispan __protobuf_metadata cache 
+            metadataCache.Put("sample_person/person.proto", File.ReadAllText("../../resources/proto2/person.proto"));
+
+            // Console.WriteLine(File.ReadAllText("../../resources/proto2/person.proto"));
+            if (metadataCache.ContainsKey(ERRORS_KEY_SUFFIX))
             {
-                IRemoteCache<Int64, Person> cache = cacheManager.GetCache<Int64, Person>();
-                
-                putPersons(cache);
-
-                QueryRequest queryRequest = new QueryRequest();
-                queryRequest.JpqlString = "from Person where firstName like '%udp%'";
-                QueryResponse queryRespone = cache.Query(queryRequest);
-                Console.WriteLine(queryRespone.NumResults);
-
+                Console.WriteLine("fail: error in registering .proto model");
+                Environment.Exit(-1);
             }
-            Console.ReadKey();
-        }
 
-        private static void putPersons(IRemoteCache<long, Person> cache)
+            testCache.Clear();
+            // Fill the application cache
+            putPersons(testCache);
+
+            // Run a query
+            QueryRequest qr = new QueryRequest();
+            qr.JpqlString = "from quickstart.Person where surname like '%ou%'";
+            QueryResponse result = testCache.Query(qr);
+
+
+            List<Person> listOfUsers = new List<Person>();
+
+
+            unwrapResults(result, listOfUsers);
+            Console.WriteLine("There are " + listOfUsers.Count + " Users:");
+
+            foreach (Person user in listOfUsers)
+            {
+                Console.WriteLine(user.ToString());
+                System.Threading.Thread.Sleep(1000);
+            }
+            System.Threading.Thread.Sleep(5000);
+
+        }
+        private static void putPersons(IRemoteCache<int, Person> cache)
         {
             Console.WriteLine(">>> Loading persons");
             string[] firstNames = File.ReadAllLines(@"..\..\First_Names.csv", Encoding.UTF8);
             string[] lastNames = File.ReadAllLines(@"..\..\Last_Names.csv", Encoding.UTF8);
-            int key = 0;
+            int key = 1;
 
             foreach (string lastName in lastNames)
             {
@@ -54,15 +80,13 @@ namespace Dulle.Education.Hotrod
                 {
                     Random randomAge = new Random();
 
-                    //IDictionary<Int64, Person> persons= new Dictionary<Int64,Person>();
-                    //persons.Add(key, new Person { Name = lastName, Age = randomAge.Next(120), FirstName = firstName });
-                    cache.Put(key, new Person { Name = lastName, Age = randomAge.Next(120), FirstName = firstName });                        
-                    
+                    cache.Put(key, new Person { Id = key, FirstName = firstName, Surname = lastName, Age = randomAge.Next(120) });
+
                     key++;
                     if ((key % 100000) == 0)
                     {
                         Console.WriteLine("  " + key);
-                        
+
                         if (key == 1000000)
                         {
                             return;
@@ -71,6 +95,34 @@ namespace Dulle.Education.Hotrod
                 }
             }
             Console.WriteLine(">>> Loading persons DONE");
+        }
+        // Convert Protobuf matter into C# objects
+        private static bool unwrapResults<T>(QueryResponse resp, List<T> res) where T : IMessage<T>
+        {
+            if (resp.ProjectionSize > 0)
+            {  // Query has select
+                return false;
+            }
+            for (int i = 0; i < resp.NumResults; i++)
+            {
+                WrappedMessage wm = resp.Results.ElementAt(i);
+
+                if (wm.WrappedBytes != null)
+                {
+                    WrappedMessage wmr = WrappedMessage.Parser.ParseFrom(wm.WrappedBytes);
+
+                    if (wmr.WrappedMessageBytes != null)
+                    {
+
+                        System.Reflection.PropertyInfo pi = typeof(T).GetProperty("Parser");
+
+                        MessageParser<T> p = (MessageParser<T>)pi.GetValue(null);
+                        T u = p.ParseFrom(wmr.WrappedMessageBytes);
+                        res.Add(u);
+                    }
+                }
+            }
+            return true;
         }
     }
 }
